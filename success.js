0d976db5ae144be221fbb443e7faa24a0e9e224f
klgvillanueva <klgvillanueva@gmail.com>
2022-05-25 15:15:39 -0400

    updated app!

--- success.js
+++ success.js
@@ -2,7 +2,7 @@ const figlet = require('figlet');
 
 // change the first argument of the call to figlet to have it print on success
 figlet.text(
-  'FTRI 7 is awesome!!!',
+  'FTRI 8 is awesome!!!',
   { horizontalLayout: 'full' },
   (err, data) => {
     if (err) {
